c28051bb74add36adfb7cbdaf05bf5623058721c
Mrdynamic-soni <ssoni5930@gmail.com>
2025-03-09 03:29:52 +0530

    added frm link

--- src/components/screens/Dashboard.jsx
+++ src/components/screens/Dashboard.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Typography from "../../components/atoms/Typography";
+import PrimaryButton from "../atoms/PrimaryButton";
+import { PST_URL } from "../../utils/urls";
+import { MdOpenInNew } from "react-icons/md";
 
 const Dashboard = () => {
-  return <div className="text-2xl font-bold">Dashboard Content</div>;
+  const handleClick = (e) => {
+    e.preventDefault();
+    window.open(`${PST_URL}/add-testimonial`, "_blank");
+  };
+  return (
+    <div className="text-2xl font-bold">
+      <div className="flex justify-start gap-x-4 items-start mt-8">
+        <Typography variant="h2">Testmonial Form :</Typography>
+        <PrimaryButton onClick={handleClick}>
+          Form <MdOpenInNew size={30} color="white" />
+        </PrimaryButton>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,2 +1,3 @@
 export const LOCAL_URL = "http://localhost:8080";
 export const BASE_URL = "https://express-app-pla4.onrender.com";
+export const PST_URL = "https://pst-sigma.vercel.app";
